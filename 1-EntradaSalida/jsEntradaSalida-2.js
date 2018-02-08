4ac09ccc201fe9ec5d34ce8656b499b7ff91d334
ayelenaldana <36243186+ayelenaldana@users.noreply.github.com>
2018-02-07 22:18:21 -0300

    Add files via upload

--- 1-EntradaSalida/jsEntradaSalida-2.js
+++ 1-EntradaSalida/jsEntradaSalida-2.js
@@ -2,6 +2,10 @@
 y luego mostrarlo por 'alert' al presionar el botón  'MOSTRAR'*/
 function Mostrar()
 {
-	
+    var dato;
+
+    dato = prompt("Ingrese un dato:");
+
+    alert ("Usted ingresó: " + dato);
 }
 
