523fc641ca49e91717ecafac4a651a8bea6d3718
ayelenaldana <36243186+ayelenaldana@users.noreply.github.com>
2018-02-09 17:50:45 -0300

    Add files via upload

--- 1-EntradaSalida/jsEntradaSalida-7.js
+++ 1-EntradaSalida/jsEntradaSalida-7.js
@@ -4,21 +4,78 @@ mostar el resulto por medio de "ALERT"
 ej.: "la Resta es 750"*/ 
 function sumar()
 {	
+    var num1;
+    var num2;
+    var sumar;
+
+    num1 = document.getElementById("numeroUno").value;
+
+    num1 = parseInt(num1);
+
+    num2 = document.getElementById("numeroDos").value;
+
+    num2 = parseInt(num2);
+
+    sumar = num1 + num2;
+
+    alert ("la suma es " + sumar);
 	
 }
 
 function restar()
 {
-	
+    var num1;
+    var num2;
+    var restar;
+
+    num1 = document.getElementById("numeroUno").value;
+
+    num1 = parseInt(num1);
+
+    num2 = document.getElementById("numeroDos").value;
+
+    num2 = parseInt(num2);
+
+    restar = num1 - num2;
+
+    alert ("la resta es " + restar);
 }
 
 function multiplicar()
 { 
-	
+	var num1;
+    var num2;
+    var multiplicar;
+
+    num1 = document.getElementById("numeroUno").value;
+
+    num1 = parseInt(num1);
+
+    num2 = document.getElementById("numeroDos").value;
+
+    num2 = parseInt(num2);
+
+    multiplicar = num1 * num2;
+
+    alert ("la multiplicación es " + multiplicar);
 }
 
 function dividir()
 {
-	
+	var num1;
+    var num2;
+    var dividir;
+
+    num1 = document.getElementById("numeroUno").value;
+
+    num1 = parseInt(num1);
+
+    num2 = document.getElementById("numeroDos").value;
+
+    num2 = parseInt(num2);
+
+    dividir = num1 / num2;
+
+    alert ("la división es " + dividir);
 }
 
--- 1-EntradaSalida/jsEntradaSalida-8.js
+++ 1-EntradaSalida/jsEntradaSalida-8.js
@@ -4,5 +4,19 @@ mostrar el resto entre el dividendo y el divisor.
 ej.: "El resto es 0 ."*/
 function SacarResto()
 {
-	
+    var num1;
+    var num2;
+    var SacarResto;
+
+    num1 = document.getElementById("numeroDividendo").value;
+
+    num1 = parseInt(num1);
+
+    num2 = document.getElementById("numeroDivisor").value;
+
+    num2 = parseInt(num2);
+
+    SacarResto = num1 % num2;
+
+    altert ("El resto es " + SacarResto);
 }
